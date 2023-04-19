a005ddb55f2e138f61fbfd6b91c7b36f334000a6
aslam48 <youngfizzy001@gmail.com>
2023-04-19 11:15:09 +0100

    working

---
+++ src/graphql/queries.js
@@ -0,0 +1,64 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      title
+      content
+      username
+      coverImage
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        title
+        content
+        username
+        coverImage
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const postByUsername = /* GraphQL */ `
+  query PostByUsername(
+    $username: String!
+    $sortDirection: ModelSortDirection
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    postByUsername(
+      username: $username
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        title
+        content
+        username
+        coverImage
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
